5fd9d33481feab8ddce2a974d31814e76f6a5080
SinanStatistician <SinanStatistician@users.noreply.github.com>
2021-02-14 21:18:43 +0300

    Initial Commit

---
+++ SwiftUIforEach/SwiftUIforEachApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIforEachApp.swift
+//  SwiftUIforEach
+//
+//  Created by Sinan Ulusan on 14.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIforEachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
